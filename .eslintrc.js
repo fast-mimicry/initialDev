e7fd33025220213dbbd0559973bedc3bb9fd4fd2
lukkymaru <qua.nyan2ify@gmail.com>
2023-02-03 20:39:34 +0900

    🐛 ESLint更新後のエラー修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,19 +6,5 @@ module.exports = {
     //この設定内で設定があるため、ecmafeaturesは不要
     "extends": [
         "eslint:recommended"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-
-    "plugins": [
-        //package.jsonの"eslint-plugin-react": "^7.31.11"が設定される
-        "react"
-    ],
-    "rules": {
-        "no-undef":"error"
-    }
+    ]
 }
