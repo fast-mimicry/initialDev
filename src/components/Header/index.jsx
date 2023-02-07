080d66493ea0ebd37c95788afd02078d6c2240c9
lukkymaru <qua.nyan2ify@gmail.com>
2023-02-07 15:53:50 +0900

    ♻️ リポジトリをPrivateからPublick変更後のプッシュテスト

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -9,11 +9,13 @@ const NAV_ITEMS = [
 export const Header = () => {
   return (
     <header className={classes.header}>
+      {/* Linkをここに書きます */}
       {NAV_ITEMS.map(item => {
         <Link key={item.href} href={item.href} className={classes.link}>
-          item.label
+          {item.label}
         </Link>
-    })}
+       })
+      }
    </header>
   );
 }
