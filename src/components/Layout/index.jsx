393936a7e5dcac7ed6a8cfd45dbbf6fd60ab4f37
fast-mimicry <qua.nyan2ify@gmail.com>
2023-03-24 09:51:08 +0900

    🐛 tailwindcssで_app.jsに適用した件の漏れを追加

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -3,8 +3,18 @@ import classes from "src/components/Layout/Layout.module.css";
 //_app.jsに上げて、全ページのルートコンテナに適用する
 export const Layout = (props) => {
   return (
-    <div className={classes.container}>
+    <div className="flex flex-col items-center px-2 min-h-screen mx-auto max-w-2xl">
       {props.children}
     </div>
   )
 }
+
+//以下をtailwindcssに置き換える
+// padding: 0 0.5rem;
+// text-align: center;
+// margin: 0 auto;
+// min-height: 100vh
+// display:flex;
+// flex-direction: column;
+// justify-content: center;
+// align-items: center;
